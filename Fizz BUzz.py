a3e0a03eace09bd1f3244357621ee6e01e5e06ea
tohka <mecg1994@gmail.com>
2019-11-28 11:46:38 -0600

    Prueba FizzBuzz
    Prueba FizzBuzz

---
+++ Fizz BUzz.py
@@ -0,0 +1,10 @@
+for var in range(1,16):
+    if var % 5 == 0 and var % 3 == 0:
+        print("fiz000buzz")
+    elif var % 3 == 0:
+        print("fizz")
+    elif var % 5 == 0:
+        print("buzz")
+    elif var :
+        print(var)
+
